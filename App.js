9dc2c3f39821340791a294eb7e1036496574bdb3
WouterVH <wouter.190@hotmail.com>
2019-12-19 16:47:31 +0100

    create redux structure

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { StyleSheet, Text, View } from "react-native";
+import { PersistGate } from "redux-persist/integration/react";
 
 import AppContainer from "./src/navigation";
+import { store, persistor } from "./src/redux/store";
 
 export default function App() {
-    return <AppContainer />;
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <AppContainer />
+            </PersistGate>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
